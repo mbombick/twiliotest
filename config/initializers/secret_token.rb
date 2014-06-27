34f6e664297f82eb7a53a1ece48b038be977c492
ZenoG <mbomb002@gmail.com>
2014-06-26 21:40:59 -0400

    intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Twiliotest::Application.config.secret_key_base = 'ee8f92047669df358a11e64d37f2b17dab88036fa82d7bb5ec15e0d64dc6f7cc61e7e3d93b9cffe148c4f2b0a553ab7af3914dbe6f35c1d5e8c1dfb6a80b5413'
